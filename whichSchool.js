cc7be9e4591181058faeaf0eb1c735f05ba53402
jaylenpatterson <jaylenpatterson16@gmail.com>
2021-11-06 21:57:39 +0000

    Initial commit with all my work so far

---
+++ whichSchool.js
@@ -0,0 +1,12 @@
+let whichSchool = function(age){
+	if(age < 13){
+	console.log("Elementary School")
+	}else if(age < 18 && age > 13){
+	console.log("Secondary School")
+	}else{
+	console.log("Lighthouse Labs")}	
+}
+
+whichSchool(35)
+whichSchool(8)
+whichSchool(14)
